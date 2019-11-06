09ca80e7f43d9ff88d4ea31188b300360ccccf16
Syed Gibran Ahmed <43906549+syedgibran96@users.noreply.github.com>
2019-11-06 19:21:27 +0530

    Add files via upload

---
+++ assets/js/Profile-Edit-Form.js
@@ -0,0 +1,41 @@
+(function(){
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar-bg').css({
+                    'background':'url('+e.target.result+')',
+                    'background-size':'cover',
+                    'background-position': '50% 50%'
+                });
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    function toggleAlert(clasz, display){
+        $(".alert")
+            .removeClass("display")
+            .removeClass("alert-info")
+            .removeClass("alert-success")
+            .removeClass("alert-danger")
+            .addClass(clasz);
+        if(display){
+            $(".alert").addClass("display")
+        }
+        if(clasz === "alert-success"){
+            $(".alert > span").text('Profile saved');
+        }else if(clasz === "alert-danger"){
+            $(".alert > span").text('Profile reset');
+        }
+    }
+
+    $("input.form-control[name=avatar-file]").change(function(){
+        readURL(this);
+    });
+    
+
+})()
